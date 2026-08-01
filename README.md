5d5d6bb5ff886b1d7dfac22a41642477e5e930b8
TeddyForce <teddyforce1437@example.com>
2026-08-01 16:06:55 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# File Organizer
+
+A simple Python program that automatically organizes files into folders based on their file type.
+
+## Features
+
+- Organizes images
+- Organizes documents
+- Organizes music
+- Organizes videos
+- Automatically creates folders if they don't exist
+
+## Skills Used
+
+- Python
+- Dictionaries
+- Loops
+- os
+- shutil
+- File Handling
+
+## How to Run
+
+```bash
+python3 main.py
+```
---
+++ main.py
@@ -0,0 +1,37 @@
+import os
+import shutil
+
+folder = "."
+
+files = os.listdir(folder)
+
+folders = {
+    "Images": [".png", ".jpg", ".jpeg"],
+    "Documents": [".pdf", ".txt", ".docx"],
+    "Music": [".mp3"],
+    "Videos": [".mp4"]
+}
+
+for folder_name in folders:
+
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
+
+for file in files:
+
+    if file ["main.py", "README.md", "LICENSE", ".gitignore":
+        continue
+
+    for folder_name in folders:
+
+        for extension in folders[folder_name]:
+
+            if file.endswith(extension):
+
+                shutil.move(file, os.path.join(folder_name, file))
+
+                print(file, "moved to", folder_name)
+
+                break
+
+print("\nDone organizing files.")
